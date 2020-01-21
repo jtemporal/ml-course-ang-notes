dfdb173c2616c42d36c0977b58a2fe08f678ed4c
Jessica Temporal <jessicatemporal@gmail.com>
2020-01-21 10:22:42 -0300

    Adds notes on basic operations

---
+++ basic_op.m
@@ -0,0 +1,27 @@
+% (89 + 94 + 69 + 72)/ 4
+
+% basic math operations
+
+10 - 2  % sub
+
+20 + 30  % sum
+
+3 * 2  % mult
+
+4 / 2  % div
+
+2^2  % power
+
+% comparisons operations
+
+1 == 2  % false     ans = 0
+
+1 ~= 2  % true      ans = 1
+
+% logic operations
+
+1 && 0  % AND       ans = 0
+
+1 || 0  % OR            ans = 1
+
+xor(1, 0) % XOR     ans = 1
