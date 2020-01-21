6dc419b300850cac77240925315eb2b0556a1a98
Jessica Temporal <jessicatemporal@gmail.com>
2020-01-21 11:59:34 -0300

    Fixing codestyle on playing_with_matrices.m

--- code/playing_with_matrices_02.m
+++ code/playing_with_matrices_02.m
@@ -1,20 +1,22 @@
-A = [ 1 2; 3 4; 5 6 ] ; % mat A
+A = [1 2; 3 4; 5 6]; % mat A
 
-A([ 1 3 ], :) ; % all the elements on rows 1 and 3
+A([1 3],:); % all the elements on rows 1 and 3
 
-A( : , 2 ) ; % all the elements of the second column
+A(:,2); % all the elements of the second column
 
-A( : , 2 )  = [10; 11; 12] ; % assign these values to the second column
+A(:,2) = [10; 11; 12]; % assign these values to the second column
 
-A = [A, [100; 101;  102]] ; % append another column to A
+A = [A, [100; 101;  102]]; % append another column to A
 
-A = [ 1 2; 3 4; 5 6 ] ; 
-B = [ 11 22; 33 44; 55 66 ] ; 
-C = [ A, B] ; % concatanates A and B, the comma is optional
+A = [1 2; 3 4; 5 6];
+B = [11, 22
+        33, 44
+        55, 66];
+C = [A, B]; % concatanates A and B, the comma is optional
 
-C = [ A; B] ; % appends B to A
+C = [A; B]; % appends B to A
 
-A(:) ; % put all elements of A into a vector
+A(:); % put all elements of A into a vector
 
 A
 B
