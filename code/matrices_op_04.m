8bc8eb756bdfc325b3d61f6ed8214e4368e65e4d
Jessica Temporal <jessicatemporal@gmail.com>
2020-01-22 20:27:41 -0300

    Adds final codes on the matrices_op.m

--- code/matrices_op_04.m
+++ code/matrices_op_04.m
@@ -7,3 +7,12 @@ A*C
 A.*B  % element-wise multiplication
 
 A.^2  % element-wise power to two
+
+A'  % is the transpose of matrix A
+
+A = magic (3); % creates a 3x3 magical matrix (all cols, rows and diagonals adds up to the same value)
+max (A);  % gets the max value column wise is the same as max(A, [], 1)
+
+max (A, [], 2);  % gets the max value row wise
+
+find (A >= 7);  % finds the values that  are greater than 4 on matrix A
